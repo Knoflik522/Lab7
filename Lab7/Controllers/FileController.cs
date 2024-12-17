dd42685be1f157e7e8331bb94e3bf08cf8cb64b1
Knoflik522 <anncheban31@gmail.com>
2024-12-17 18:45:00 +0200

    Add project files.

---
+++ Lab7/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace Lab7.Controllers
+{
+    public class FileController : Controller
+    {
+        // GET: File/DownloadFile
+        [HttpGet]
+        public IActionResult DownloadFile()
+        {
+            return View();
+        }
+
+        // POST: File/DownloadFile
+        [HttpPost]
+        public IActionResult DownloadFile(string firstName, string lastName, string fileName)
+        {
+            // Перевірка: Якщо ім'я файлу не введено - дефолтне значення
+            if (string.IsNullOrEmpty(fileName))
+            {
+                fileName = "DefaultFileName";
+            }
+
+            
+            var fileContent = $"Ім'я: {firstName}\nПрізвище: {lastName}";
+            var bytes = Encoding.UTF8.GetBytes(fileContent);
+
+            
+            return File(bytes, "text/plain", $"{fileName}.txt");
+        }
+    }
+}
